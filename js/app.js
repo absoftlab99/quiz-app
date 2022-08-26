f9cbfcbee634650e60212a1a42fed6acd6fc9401
Abdullah Al Mahmud <58851539+absoftlab99@users.noreply.github.com>
2022-08-26 12:38:52 +0600

    quiz progress bar setup complete

--- js/app.js
+++ js/app.js
@@ -1,4 +1,17 @@
 const progress = document.getElementById('progress');
-const width = 0;
-const quizNo = 1;
-progress.style.width = `${width}%`;
+progress.style.width = 0;
+let width = 0;
+let qnumber = 0;
+document.getElementById('next').addEventListener('click', function(){
+    width = width + 20;
+    const progress = document.getElementById('progress');
+    progress.style.width = `${width}%`;
+    qnumber = qnumber + 1;
+    if(qnumber <= 5){
+        const qNum = document.getElementById('q-no');
+        qNum.innerText = `${qnumber}`;
+    }
+    else{
+        alert('No more question have')
+    }
+})
